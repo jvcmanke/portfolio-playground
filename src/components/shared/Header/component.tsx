893aa35786b2439a8bddbee2ccd260b1d065fed4
João Victor Manke <jvcmanke@gmail.com>
2022-07-04 21:48:54 -0300

    Create header button

--- src/components/shared/Header/component.tsx
+++ src/components/shared/Header/component.tsx
@@ -6,22 +6,29 @@ export type HeaderProps = {};
 export const Header: React.FC<HeaderProps> = () => {
   return (
     <header className="fixed top-0 right-0 left-0 grid grid-cols-2 py-4 px-8 content-center bg-black text-white z-50">
-      <div className="flex gap-2 items-center">
-        <h1 className="text-lg leading-none">João Victor Manke</h1>
-        <h3 className="text-sm self-end">Full Stack Software Developer</h3>
+      <div className="flex gap-2 items-baseline self-center">
+        <h1 className="text-lg">João Victor Manke</h1>
+        <h3 className="text-sm">Full Stack Software Developer</h3>
       </div>
-      <nav className="justify-self-end">
-        <ul className="flex gap-4">
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="#top" scroll>
-              ARROW
-            </Link>
-          </li>
-        </ul>
-      </nav>
+      <div className="justify-self-end flex gap-4 items-center">
+        <nav>
+          <ul className="flex gap-4">
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="#top" scroll>
+                ARROW
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <a href="/assets/CV.pdf" download="Resume-JoaoVictorManke.pdf">
+          <button className="rounded-full bg-slate-600 hover:bg-slate-500 shadow-lg transition-colors px-4 py-1">
+            Resume
+          </button>
+        </a>
+      </div>
     </header>
   );
 };
