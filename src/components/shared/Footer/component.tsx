4a2e4ac34729654631587c2f39d191eec5542111
João Victor Manke <jvcmanke@gmail.com>
2022-07-04 22:19:35 -0300

    Add links to footer

--- src/components/shared/Footer/component.tsx
+++ src/components/shared/Footer/component.tsx
@@ -5,6 +5,43 @@ export type FooterProps = {};
 export const Footer: React.FC<FooterProps> = () => {
   return (
     <footer className="bg-black text-white">
+      <div className="grid grid-cols-3 content-center justify-items-center py-8 px-8">
+        <div />
+        <div />
+        <div className="text-center">
+          <h4 className="text-lg font-bold">Useful links:</h4>
+          <ul>
+            <li>
+              <a
+                className="underline hover:text-gray-400"
+                href="https://github.com/jvcmanke"
+                rel="noreferrer"
+                target="_blank"
+              >
+                GitHub
+              </a>
+            </li>
+            <li>
+              <a
+                className="underline hover:text-gray-400"
+                href="/assets/CV.pdf"
+                download="Resume-JoaoVictorManke.pdf"
+              >
+                Resume
+              </a>
+            </li>
+            <li>
+              <a
+                className="underline hover:text-gray-400"
+                href="/assets/settings.json"
+                download="settings.json"
+              >
+                VSCode Settings
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
       <p className="text-center py-1 px-2 text-sm">
         Created by João Victor Manke
       </p>
