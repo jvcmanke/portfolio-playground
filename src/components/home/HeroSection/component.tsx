d45e2a32e8dfcd40418a13457daa523de69753ae
João Victor Manke <jvcmanke@gmail.com>
2022-07-09 15:03:35 -0300

    Make hero section responsive

--- src/components/home/HeroSection/component.tsx
+++ src/components/home/HeroSection/component.tsx
@@ -12,17 +12,21 @@ export const HeroSection: React.FC<HeroSectionProps> = ({ nextSectionId }) => {
   return (
     <section
       id="heroSection"
-      className="relative flex h-screen items-center justify-center gap-24"
+      className="relative flex h-screen items-center justify-center gap-24 flex-col sm:flex-row"
     >
-      <div>
-        <h1 className="text-5xl font-extrabold">Hi! My name is João,</h1>
-        <h2 className="text-3xl">I&apos;m a Full Stack developer!</h2>
-        <p className="mt-12 text-xl">
+      <div className="order-2 sm:order-1 text-center sm:text-left">
+        <h1 className="text-4xl font-extrabold sm:text-5xl">
+          Hi! My name is João,
+        </h1>
+        <h2 className="text-2xl sm:text-3xl">
+          I&apos;m a Full Stack developer!
+        </h2>
+        <p className="mt-12 sm:text-xl text-lg">
           I also love to learn and share knowledge whenever I can! <br /> Keep
           going to lean more about me!
         </p>
       </div>
-      <div className="relative h-60 w-60">
+      <div className="relative h-60 w-60 order-1 sm:order-2">
         <Image
           className="rounded-full"
           alt="My Profile Pic"
@@ -30,7 +34,7 @@ export const HeroSection: React.FC<HeroSectionProps> = ({ nextSectionId }) => {
           layout="fill"
         />
       </div>
-      <div className="absolute bottom-8">
+      <div className="absolute bottom-8 hidden sm:block">
         <Link href={`#${nextSectionId}`} scroll>
           <ChevronDownIcon className="h-10 w-10 cursor-pointer text-black hover:opacity-75" />
         </Link>
